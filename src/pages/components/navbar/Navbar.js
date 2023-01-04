5fd177f75fb054f88858f1ecfec27171712ed1bc
Harish Chandra Mishra <h.c.mishra512@gmail.com>
2023-01-04 20:30:51 +0530

    first commit

---
+++ src/pages/components/navbar/Navbar.js
@@ -0,0 +1,59 @@
+import "./Navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import { DarkModeContext } from "../../../context/darkModeContext";
+import { useContext } from "react";
+
+const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" name="" id="" placeholder="search..." />
+          <SearchIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon" />
+            English
+          </div>
+          <div className="item">
+            <DarkModeIcon
+              className="icon"
+              onClick={() => dispatch({ type: "TOGGLE" })}
+            />
+          </div>
+          <div className="item">
+            <FullscreenExitIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsIcon className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListAltIcon className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/components/sidebar/Sidebar.js
@@ -0,0 +1,104 @@
+import "./Sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
+import QrCodeIcon from "@mui/icons-material/QrCode";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+import LoginIcon from "@mui/icons-material/Login";
+import SettingsIcon from "@mui/icons-material/Settings";
+import PortraitIcon from "@mui/icons-material/Portrait";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../../context/darkModeContext";
+import { useContext } from "react";
+
+const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Admin Panel</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <div className="title">MAIN</div>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <div className="title">LISTS</div>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <QrCodeIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
+          <li>
+            <CreditCardIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <div className="title">USEFULL</div>
+          <li>
+            <QueryStatsIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsActiveIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <div className="title">SERVICE</div>
+          <li>
+            <MonitorHeartIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <LoginIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <div className="title">USER</div>
+          <li>
+            <PortraitIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="screen_mode">SCREEN DISPLAY</div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
